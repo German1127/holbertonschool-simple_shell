8a52a20915cb0a75615044ca1db7c3f6a8af1ddf
German1127 <kleverk2023@gmail.com>
2023-08-15 09:27:57 -0700

    update main.h

--- main.h
+++ main.h
@@ -1,16 +1,21 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <stddef.h>
 
-char *path(char *filename);
-char *_getenv(const char *name);
 char *findexpath(const char *command, const char *path);
-int main();
+char *_getenv(const char *name);
+void executeCommand(char *command, char **arraytok);
+int main(void);
+char **strtok_str(char *copy_line);
+char *readinput();
+void free_memory(char *lineptr, char *copy_line, char **args);
 
 #endif
